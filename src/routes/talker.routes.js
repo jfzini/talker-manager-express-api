506eeef89a35b2831d5c9d4f7f57509753c86ef7
João Felipe Zini <joaofelipezini@gmail.com>
2023-07-20 15:10:14 -0300

    2nd challenge

--- src/routes/talker.routes.js
+++ src/routes/talker.routes.js
@@ -7,4 +7,13 @@ talkerRouter.get('/', async (req, res) => {
   res.status(200).json(data);
 });
 
+talkerRouter.get('/:id', async (req, res) => {
+  const data = await readFile();
+  const { id } = req.params;
+  const talker = data.find((person) => person.id === Number(id))
+  if (talker) return res.status(200).json(talker);
+
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
 module.exports = talkerRouter;
